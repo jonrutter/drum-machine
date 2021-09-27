6411476f468401beff503eecf62578020973786e
jonrutter <contact@jonrutter.com>
2021-09-27 16:02:58 -0400

    App data file

---
+++ src/data.js
@@ -0,0 +1,116 @@
+// all audio source is from freeCodeCamp:
+// https://codepen.io/freeCodeCamp/pen/MJyNMd
+
+export const bankHeater = [
+  {
+    keyCode: 81,
+    keyName: 'Q',
+    description: 'Heater 1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+  },
+  {
+    keyCode: 87,
+    keyName: 'W',
+    description: 'Heater 2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+  },
+  {
+    keyCode: 69,
+    keyName: 'E',
+    description: 'Heater 3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+  },
+  {
+    keyCode: 65,
+    keyName: 'A',
+    description: 'Heater 4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+  },
+  {
+    keyCode: 83,
+    keyName: 'S',
+    description: 'Clap',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+  },
+  {
+    keyCode: 68,
+    keyName: 'D',
+    description: 'Open HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+  },
+  {
+    keyCode: 90,
+    keyName: 'Z',
+    description: 'Kick and Hat',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+  },
+  {
+    keyCode: 88,
+    keyName: 'X',
+    description: 'Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+  },
+  {
+    keyCode: 67,
+    keyName: 'C',
+    description: 'Closed HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+  },
+];
+
+export const bankPiano = [
+  {
+    keyCode: 81,
+    keyName: 'Q',
+    description: 'Chord 1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3',
+  },
+  {
+    keyCode: 87,
+    keyName: 'W',
+    description: 'Chord 2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3',
+  },
+  {
+    keyCode: 69,
+    keyName: 'E',
+    description: 'Chord 3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3',
+  },
+  {
+    keyCode: 65,
+    keyName: 'A',
+    description: 'Shaker',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3',
+  },
+  {
+    keyCode: 83,
+    keyName: 'S',
+    description: 'Open HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3',
+  },
+  {
+    keyCode: 68,
+    keyName: 'D',
+    description: 'Closed HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3',
+  },
+  {
+    keyCode: 90,
+    keyName: 'Z',
+    description: 'Punchy Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3',
+  },
+  {
+    keyCode: 88,
+    keyName: 'X',
+    description: 'Side Stick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3',
+  },
+  {
+    keyCode: 67,
+    keyName: 'C',
+    description: 'Snare',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3',
+  },
+];
